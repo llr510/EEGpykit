377541df925b6ad6acefe7f56d2e683137acb27e
llr510 <lyndon.rakusen@york.ac.uk>
2023-05-19 17:37:04 +0100

    export_to_brainstorm now writes csv with all epoch events. Also added Brainstorm .m script for applying event labels to imported data.

--- utils/export_to_brainstorm.py
+++ utils/export_to_brainstorm.py
@@ -2,11 +2,31 @@ from pathlib import Path
 import mne
 from preprocesser import EEG_Participant
 import pickle
+import pandas as pd
 
+
+def recode_label(event):
+    labels = ['Rate', 'View',
+              'Global', 'Obvious', 'Subtle', 'Normal',
+              'Correct', 'Incorrect', 'Missed',
+              'PerceivedAbnormal', 'PerceivedN']
+
+    event = event.replace('PerceivedNormal', 'PerceivedN')
+    label_list = []
+    for label in labels:
+        if label in event:
+            label_list.append(label)
+    tags = ' | '.join(label_list)
+    tags = tags.replace('PerceivedN', 'PerceivedNormal')
+    return tags
+
+
+n = 5
 inf = Path(
-    '/Volumes/psgroups/AttentionPerceptionLab/AttentionPerceptionLabStudent/PROJECTS/EEG_MAMMO_EXPERMENT/MNE_preprocessing_db/EEGTraining_Rad1/EEGTraining_Rad1_epochs_output.pickle')
+    f'/Volumes/psgroups-1/AttentionPerceptionLab/AttentionPerceptionLabStudent/PROJECTS/EEG_MAMMO_EXPERMENT'
+    f'/MNE_preprocessing_db/EEGTraining_Rad{n}/EEGTraining_Rad{n}_epochs_output.pickle')
 
-outf = Path('test-epo.fif')
+outf = inf.with_suffix('.fif')
 
 with open(inf, 'rb') as f:
     dat = pickle.load(f)
@@ -14,16 +34,22 @@ with open(inf, 'rb') as f:
 if type(dat) == dict:
     epochs = dat['all']
 
-print(epochs)
-print(epochs.events)
-print(epochs.event_id)
+ids = epochs.events[:, 2]
+assert len(ids) == len(epochs)
+labels = epochs.event_id
+labels = {v: k for k, v in labels.items()}
+
+tags = []
+epochs_n = []
+for i in range(len(ids)):
+    epochs_n.append(i + 1)
+    event = labels[ids[i]]
+
+    tag = recode_label(event)
+    tags.append(tag)
+
+df = pd.DataFrame({'epoch': epochs_n, 'label': tags})
+df.to_csv(inf.with_suffix('.csv'), index=False)
+
 epochs.times.flags['WRITEABLE'] = False
 epochs.save(outf, overwrite=True)
-
-# mne.write_events('events-eve.fif', epochs.events, overwrite=True)
-# mne.write_events('events-eve.txt', epochs.events, overwrite=True)
-
-# e = mne.read_epochs(outf)
-#
-# print(e)
-# print(e.event_id)
